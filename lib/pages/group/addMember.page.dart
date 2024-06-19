54e2bb234446ebb393145840ae391f7d69190fd7
Lucas Norio <williankenzokazama@gmail.com>
2024-06-18 21:30:43 -0300

    ultimo commit

--- lib/pages/group/addMember.page.dart
+++ lib/pages/group/addMember.page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:go_router/go_router.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -48,11 +49,16 @@ class _AddMemberState extends State<AddMember> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Convidar membros"),
+          title: Row(children: [
+            IconButton(
+                onPressed: () => GoRouter.of(context).go("/home"),
+                icon: const Icon(Icons.arrow_back)),
+            const Text("Convidar membros")
+          ]),
         ),
         body: Container(
-          padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
-          decoration: BoxDecoration(
+          padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
+          decoration: const BoxDecoration(
               image: DecorationImage(
             image: AssetImage("images/HomeBackground.png"),
             fit: BoxFit.fill,
@@ -64,16 +70,16 @@ class _AddMemberState extends State<AddMember> {
                   child: Column(
                     children: [
                       Container(
-                          margin: EdgeInsets.only(bottom: 20),
+                          margin: const EdgeInsets.only(bottom: 20),
                           width: 350,
-                          child: Text("Insira o e-mail do convidado",
+                          child: const Text("Insira o e-mail do convidado",
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                   height: 1,
                                   fontSize: 25,
                                   color: Colors.white))),
                       TextField(
-                          style: TextStyle(color: Colors.black),
+                          style: const TextStyle(color: Colors.black),
                           maxLength: 8,
                           decoration: InputDecoration(
                             hintText: "Digite o e-mail",
@@ -85,27 +91,88 @@ class _AddMemberState extends State<AddMember> {
                           ),
                           keyboardType: TextInputType.emailAddress),
                       Container(
-                        margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                        margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
                         width: 200,
                         height: 50,
                         child: OutlinedButton(
-                          child: Text(
-                            "CONVIDAR",
-                            style: TextStyle(
-                                height: 1, fontSize: 20, color: Colors.white),
-                          ),
                           style: OutlinedButton.styleFrom(
-                              side: BorderSide(
+                              side: const BorderSide(
                                 width: 3.0,
                                 color: Colors.white,
                               ),
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(20))),
                           onPressed: () => _onEntry(context),
+                          child: const Text(
+                            "CONVIDAR",
+                            style: TextStyle(
+                                height: 1, fontSize: 20, color: Colors.white),
+                          ),
                         ),
                       ),
+                      // Container(
+                      //   padding: const EdgeInsets.all(30),
+                      //   width: 500,
+                      //   height: 270,
+                      //   decoration: BoxDecoration(
+                      //       border: Border.all(color: Colors.white),
+                      //       borderRadius: BorderRadius.circular(20)),
+                      //   child: const Column(
+                      //     children: [
+                      //       // Row(
+                      //       //   children: [
+                      //       //     Icon(
+                      //       //       Icons.link,
+                      //       //       size: 45,
+                      //       //     ),
+                      //       //     SizedBox(
+                      //       //       width: 30,
+                      //       //     ),
+                      //       //     Text("Link do Grupo",
+                      //       //         style: TextStyle(fontSize: 25))
+                      //       //   ],
+                      //       // ),
+                      //       // const SizedBox(
+                      //       //   height: 40,
+                      //       // ),
+                      //       // TextField(
+                      //       //   decoration: InputDecoration(
+                      //       //     border: OutlineInputBorder(
+                      //       //         borderRadius: BorderRadius.circular(20)),
+                      //       //     filled: true,
+                      //       //     fillColor: Colors.white,
+                      //       //     counterText: "",
+                      //       //   ),
+                      //       //   style: const TextStyle(color: Colors.black),
+                      //       //   controller: groupLink,
+                      //       //   enabled: false,
+                      //       // ),
+                      //       // const SizedBox(height: 30),
+                      //       // TextButton(
+                      //       //     onPressed: () {
+                      //       //       Clipboard.setData(
+                      //       //           ClipboardData(text: groupLink.text));
+                      //       //       ScaffoldMessenger.of(context).showSnackBar(
+                      //       //         const SnackBar(
+                      //       //           content: Text('Copiado!'),
+                      //       //           backgroundColor:
+                      //       //               Color.fromRGBO(70, 29, 115, 1),
+                      //       //           duration: Duration(seconds: 3),
+                      //       //         ),
+                      //       //       );
+                      //       //     },
+                      //       //     child: const Text(
+                      //       //       "Copiar",
+                      //       //       style: TextStyle(fontSize: 20),
+                      //       //     )),
+                      //     ],
+                      //   ),
+                      // ),
+                      // const SizedBox(
+                      //   height: 30,
+                      // ),
                       Container(
-                        padding: EdgeInsets.all(30),
+                        padding: const EdgeInsets.all(30),
                         width: 500,
                         height: 270,
                         decoration: BoxDecoration(
@@ -113,68 +180,7 @@ class _AddMemberState extends State<AddMember> {
                             borderRadius: BorderRadius.circular(20)),
                         child: Column(
                           children: [
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.link,
-                                  size: 45,
-                                ),
-                                SizedBox(
-                                  width: 30,
-                                ),
-                                Text("Link do Grupo",
-                                    style: TextStyle(fontSize: 25))
-                              ],
-                            ),
-                            SizedBox(
-                              height: 40,
-                            ),
-                            TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(20)),
-                                filled: true,
-                                fillColor: Colors.white,
-                                counterText: "",
-                              ),
-                              style: TextStyle(color: Colors.black),
-                              controller: groupLink,
-                              enabled: false,
-                            ),
-                            SizedBox(height: 30),
-                            TextButton(
-                                onPressed: () {
-                                  Clipboard.setData(
-                                      ClipboardData(text: groupLink.text));
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                      content: Text('Copiado!'),
-                                      backgroundColor:
-                                          Color.fromRGBO(70, 29, 115, 1),
-                                      duration: const Duration(seconds: 3),
-                                    ),
-                                  );
-                                },
-                                child: Text(
-                                  "Copiar",
-                                  style: TextStyle(fontSize: 20),
-                                )),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(30),
-                        width: 500,
-                        height: 270,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Column(
-                          children: [
-                            Row(
+                            const Row(
                               children: [
                                 Icon(
                                   Icons.code,
@@ -187,7 +193,7 @@ class _AddMemberState extends State<AddMember> {
                                     style: TextStyle(fontSize: 25))
                               ],
                             ),
-                            SizedBox(
+                            const SizedBox(
                               height: 40,
                             ),
                             TextField(
@@ -198,27 +204,30 @@ class _AddMemberState extends State<AddMember> {
                                 fillColor: Colors.white,
                                 counterText: "",
                               ),
-                              style: TextStyle(color: Colors.black),
+                              style: const TextStyle(color: Colors.black),
                               controller: groupCode,
                               enabled: false,
                             ),
-                            SizedBox(height: 30),
+                            const SizedBox(
+                              height: 16,
+                              width: 20,
+                            ),
                             TextButton(
                                 onPressed: () {
                                   Clipboard.setData(
                                       ClipboardData(text: groupCode.text));
                                   ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
+                                    const SnackBar(
                                       content: Text('Copiado!'),
                                       backgroundColor:
                                           Color.fromRGBO(70, 29, 115, 1),
-                                      duration: const Duration(seconds: 3),
+                                      duration: Duration(seconds: 3),
                                     ),
                                   );
                                 },
-                                child: Text(
+                                child: const Text(
                                   "Copiar",
-                                  style: TextStyle(fontSize: 20),
+                                  style: TextStyle(fontSize: 14),
                                 )),
                           ],
                         ),
